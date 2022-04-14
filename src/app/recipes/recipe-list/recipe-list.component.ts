43ad317c13cd5bba00de57d64b156ab286ae3372
rajan <rajan@simformsolutions.com>
2022-04-14 19:35:32 +0530

    feat: added basic recipes and shopping components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Undhiyu Chapdi',
+      'Fav kathiywadi Dish, also known as TAVO',
+      'https://3.bp.blogspot.com/-RJ7n9Xrs6HU/XlXYfgd1-QI/AAAAAAAAv_g/HHu3mFTeENw19NlEWqssN98c3u-w2b2qgCK4BGAYYCw/s1600/IMG_0866-002.jpg'
+    ),
+    new Recipe(
+      'Undhiyu Chapdi',
+      'Fav kathiywadi Dish, also known as TAVO',
+      'https://3.bp.blogspot.com/-RJ7n9Xrs6HU/XlXYfgd1-QI/AAAAAAAAv_g/HHu3mFTeENw19NlEWqssN98c3u-w2b2qgCK4BGAYYCw/s1600/IMG_0866-002.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
